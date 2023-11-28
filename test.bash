92e4b7d031772f045fa8f3d8733aac9e32f82210
Xe <a@hoge.com>
2023-11-28 23:01:45 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -14,5 +14,9 @@ out=$(seq 5 | ./plus)
 echo "Output: ${out}"
 [ "${out}" = "15,-15" ] || ng ${LINENO}
 
+out=$(echo -e "1\n2\n-1\n0.1" | python3 plus)
+echo "Output: ${out}"
+[ "${out}" = "2.1,-2.1" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
