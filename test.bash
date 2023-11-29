4ed769be32c67d141d6032d014945d66475af7d0
Xe <a@hoge.com>
2023-11-29 14:49:52 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -18,5 +18,14 @@ out=$(echo -e "1\n2\n-1\n0.1" | python3 plus) #マイナスと小数点を含め
 echo "Output: ${out}"
 [ "${out}" = "2.1,-2.1" ] || ng ${LINENO}
 
+### 例外処理 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
